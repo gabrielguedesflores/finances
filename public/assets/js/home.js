34c491f415fbfa4cec938e82e25b6ddd8bc3ff71
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-07-08 20:39:53 -0300

    iniciando mudança de layout

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
-  $('#main').hide()
-  $('.progress').show()
-  
-  setTimeout(function(){
-    $('#main').show()
-    $('.progress').hide()
-  }, 1000)
+  loading()
+  binds()
+  handler()
+});
 
+const loading = () => {
+  $('.progress').show()
+  $('#main').hide()
+  $('#footer').hide()
+
+  setTimeout(function(){
+    $('.progress').hide()
+    $('#main').show()
+    $('#footer').show()
+  }, 1000)
+}
+
+const binds = () => {
   $('.collapsible').collapsible();
   $(".button-collapse").sideNav();
   $(".button-collapse").css('margin', '5px');
-  
-});
+}
+
+const handler = () => {
+  $('#btnLayoutGrid').on('click', () => {
+    console.log('btnLayoutGrid');
+    $('#list').hide('slow')
+    $('#grid').show('slow')
+  })
+
+  $('#btnLayoutList').on('click', () => {
+    console.log('btnLayoutList');
+    $('#list').show('slow')
+    $('#grid').hide('slow')
+  })
+}
