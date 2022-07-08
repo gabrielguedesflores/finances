a67b3ab854b639a6c1957d66faa133186186055d
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-07-07 23:42:50 -0300

    configurando primeiros elementos

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  $('.collapsible').collapsible();
+  $('#main').hide()
+  $('.progress').show()
+  
+  setTimeout(function(){
+    $('#main').show()
+    $('.progress').hide()
+  }, 1000)
 
+  $('.collapsible').collapsible();
   $(".button-collapse").sideNav();
+  $(".button-collapse").css('margin', '5px');
+  
 });
