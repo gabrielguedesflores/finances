f34264e9b75d13f71e24866f34f1afde765aee31
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-07-13 23:29:16 -0300

    createLabel via js

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
   binds()
+  handlersHome()
   calledMaterialize()
 });
 
 const binds = () => {
   $("#labelValue").mask('R$ #.##0,00')
-  $("#labelValue").val('R$ 1.561,66')
 }
 
 const calledMaterialize = () => {
   $('.modal').modal();
-
-  $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
+  $('.chips').on('chip.select', function(e, chip){
+    console.log('handlePaid');
+  });
 }
 
+const handlersHome = () => {
+  $('#labelpaid').on('click', handlePaid())
+}
+
+const handlePaid = () => {
+  console.log('handlePaid');
+}
