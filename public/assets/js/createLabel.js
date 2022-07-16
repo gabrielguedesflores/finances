c10179214d804254feeb289ececc79239c451923
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-07-15 22:05:15 -0300

    checkpoiint

--- public/assets/js/createLabel.js
+++ public/assets/js/createLabel.js
@@ -31,15 +31,86 @@ const buildLabel = (label) => {
       </div>
       <div class="collapsible-body">
         <span>R$ ${label.labelvalue}</span>
-        <a href="#modal1" style="float: right;" class="black-text modal-trigger" onClick=""><i class="material-icons">create</i></a>
+        <a href="#modal${label.labelid}" style="float: right;" class="black-text modal-trigger"><i class="material-icons">create</i></a>
       </div>
     </li>`
+}
+
+const buildModal = (label) => {
+  const labelpaidAux = label.labelpaid == true ? 'teal white-text' : 'black-text'
+  return `
+  <div id="modal${label.labelid}" class="modal">
+    <form>
+      <div class="input-field" style="display: none;">
+        <input id="labelid" type="text" value="${label.labelid}">
+      </div>
+      <div class="modal-content">
+        <h2>
+          <div class="row col s12">
+            <div class="input-field">
+              <h2>
+                <input id="labeltitle" type="text" class="validate" value="${label.labeltitle}">
+                <label for="">Título:</label>
+              </h2>
+            </div>
+          </div> 
+        </h2>
+        <div class="divider"></div><br>
+        <div class="input-field">
+          <label for="first_name">Descrição:</label>
+          <textarea id="labeldesc" class="materialize-textarea">${label.labeldesc}</textarea>
+        </div>
+        <div class="divider"></div><br>
+        <a href="#!" id="labelpaid">
+          <div class="chip ${labelpaidAux}">
+            Paga
+          </div>
+        </a>
+        <br /><br />
+        <div class="divider"></div><br>
+        <div id="category" action="#">
+          <p>Categorias:</p>
+          <p>
+            <div class="chip black-text col s3" style="text-align: center;">
+              <input class="with-gap" name="createCategoryLabel" class="red" type="radio" id="1" />
+              <label for="1" class="black-text">Casa</label>
+            </div>
+            <div class="chip black-text col s3" style="text-align: center;">
+              <input class="with-gap" name="createCategoryLabel" type="radio" id="2" />
+              <label for="2" class="black-text">Mercado</label>
+            </div>
+            <div class="chip black-text col s3" style="text-align: center;">
+              <input class="with-gap" name="createCategoryLabel" type="radio" id="3"  />
+              <label for="3" class="black-text">Lazer</label>
+            </div>               
+            <div class="chip black-text col s3" style="text-align: center;">
+              <input class="with-gap" name="createCategoryLabel" type="radio" id="4"  />
+              <label for="4" class="black-text">Transporte</label>
+            </div>               
+          </p>
+        </div>
+        <div class="divider"></div><br>
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="labelvalue" type="text" class="validate" value="${label.labelvalue}">
+            <label for="">Valor</label>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a class="modal-action modal-close waves-effect waves-green btn-flat">Sair</a>
+        <a class="waves-effect waves-green btn-flat" id="btnSaveLabel">Salvar</a>
+      </div>
+    </form>
+  </div>    
+  `
 }
 
 const initLabel = async(userid) => {
   $('#loadingLabel').show()
   const label = await getLabel(userid)
   for (let i = 0; i < label.length; i++) {
+    $('#modais').append(buildModal(label[i]))
     $('.collapsible').append(buildLabel(label[i]))
   }
   console.log(label);
